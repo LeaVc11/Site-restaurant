27216dfb69281529ef688990454ace241d5d4f8c
Carine V <77583125+LeaVc11@users.noreply.github.com>
2024-02-01 16:19:24 +0100

    fini le JS

--- script.js
+++ script.js
@@ -9,31 +9,46 @@ navToogler.addEventListener("click", toogleNav)
 
 function toogleNav() {
     nav.classList.toggle("active");
-    if(tooglerImage.src.includes("hamburger")) {
+    if (tooglerImage.src.includes("hamburger")) {
         tooglerImage.src = "ressources/croix.svg";
         navToogler.ariaExpanded = true;
-    }else{
+    } else {
         tooglerImage.src = "ressources/hamburger.svg";
         navToogler.ariaExpanded = false;
     }
 }
+
 /*
 -------------------------------------------
 */
 // slideshow
-const slideshowImg = document.querySelectorAll(".slideshow-images-container img");
-const fadeSlideDots = document.querySelectorAll(".fade-slide-dots.dot");
+const slideshowImages = document.querySelectorAll(".slideshow-images-container img");
+const fadeSlideDots = document.querySelectorAll(".fade-slide-dots .dot");
 
-fadeSlideDots.forEach(dot =>dot.addEventListener("click", fadeSlideShow))
+fadeSlideDots.forEach(dot => dot.addEventListener("click", fadeSlideshow))
+
 let currentFadeIndex = 1;
 let fadeIntervalID;
-function fadeSlideShow(event) {
-    slideshowImg[currentFadeIndex -1].classList.remove("active");
-    fadeSlideDots[currentFadeIndex -1].classList.remove("active");
-    fadeSlideDots[currentFadeIndex -1].ariaDisabled = "false";
-    if(e){
-        currentFadeIndex = e.target.getAttribute(("data-fadeIndex"));
-    }else{
 
-    }
+function fadeSlideshow(e) {
+
+    slideshowImages[currentFadeIndex - 1].classList.remove("active");
+    fadeSlideDots[currentFadeIndex - 1].classList.remove("active");
+    fadeSlideDots[currentFadeIndex - 1].ariaDisabled = "false";
+
+    if (e) {
+        currentFadeIndex = e.target.getAttribute("data-fadeIndex");
+        clearInterval(fadeIntervalID)
+        fadeIntervalID = window.setInterval(fadeSlideshow, 3500)
+    } else {
+currentFadeIndex++;
+if (currentFadeIndex > slideshowImages.length){
+    currentFadeIndex = 1;
 }
+    }
+    slideshowImages[currentFadeIndex - 1].classList.add("active");
+    fadeSlideDots[currentFadeIndex - 1].classList.add("active");
+    fadeSlideDots[currentFadeIndex - 1].ariaDisabled = "true";
+}
+
+fadeIntervalID = window.setInterval(fadeSlideshow, 3500)
